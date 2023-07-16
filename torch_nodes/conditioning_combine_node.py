c48477265dcfdae2a514836997c3a73f8fd4397e
unknown <miklos.mnagy@gmail.com>
2023-07-16 21:30:18 +0200

    cond combine upd

--- torch_nodes/conditioning_combine_node.py
+++ torch_nodes/conditioning_combine_node.py
@@ -89,15 +89,17 @@ class ConditioningCombineNode(AiNode):
 
 
 
-                c = self.addWeighted(cond1_list[0], cond2_list[0], strength)
+                c = self.addWeighted(cond1_list["conds"], cond2_list["conds"], strength)
                 if gs.logging:
                     print("COND COMBINE NODE: Conditionings weighted.")
-                return [c]
+                return c
             else:
                 conds_list_length = self.content.cond_list_length.value()
                 if conds_list_length > 1:
 
                     if isinstance(cond1_list, dict):
+
+
                         cond1_list = cond1_list["conds"]
 
                         if len(cond1_list) == 1:
@@ -154,6 +156,33 @@ class ConditioningCombineNode(AiNode):
 
         return blended_conditionings
     def addWeighted(self, conditioning_to, conditioning_from, conditioning_to_strength):
+        out = []
+
+        if len(conditioning_from) > 1:
+            print("Warning: ConditioningAverage conditioning_from contains more than 1 cond, only the first one will actually be applied to conditioning_to.")
+
+        cond_from = conditioning_from[0][0]
+        pooled_output_from = conditioning_from[0][1].get("pooled_output", None)
+
+        for i in range(len(conditioning_to)):
+            t1 = conditioning_to[i][0]
+            pooled_output_to = conditioning_to[i][1].get("pooled_output", pooled_output_from)
+            t0 = cond_from[:,:t1.shape[1]]
+            if t0.shape[1] < t1.shape[1]:
+                t0 = torch.cat([t0] + [torch.zeros((1, (t1.shape[1] - t0.shape[1]), t1.shape[2]))], dim=1)
+
+            tw = torch.mul(t1, conditioning_to_strength) + torch.mul(t0, (1.0 - conditioning_to_strength))
+            t_to = conditioning_to[i][1].copy()
+            if pooled_output_from is not None and pooled_output_to is not None:
+                t_to["pooled_output"] = torch.mul(pooled_output_to, conditioning_to_strength) + torch.mul(pooled_output_from, (1.0 - conditioning_to_strength))
+            elif pooled_output_from is not None:
+                t_to["pooled_output"] = pooled_output_from
+
+            n = [tw, t_to]
+            out.append(n)
+        outback = [[out[0][0], {"pooled_output": out[0][1]["pooled_output"]}]]
+        return outback
+    def addWeighted_(self, conditioning_to, conditioning_from, conditioning_to_strength):
         out = []
 
         if len(conditioning_from) > 1:
