c7a77450ceb60e1794e7c21ee7031c4e613277ec
Miklos Nagy <miklos.mnagy@gmail.com>
2023-08-13 18:36:13 +0200

    Update diffusers_xl_node.py

--- diffusers_nodes/diffusers_xl_node.py
+++ diffusers_nodes/diffusers_xl_node.py
@@ -43,7 +43,6 @@ class DiffBaseXLWidget(QDMNodeContentWidget):
         self.target_width = self.create_spin_box("Target width", min_val=64, max_val=4096, default_val=1024, step=64)
         self.target_height = self.create_spin_box("Target height", min_val=64, max_val=4096, default_val=1024, step=64)
 
-
         self.create_main_layout(grid=1)
 
 @register_node(OP_NODE_DIFFBASE_XL)
@@ -58,6 +57,7 @@ class DiffBaseXLNode(AiNode):
     dim = (340, 1152)
     output_data_ports = [0,1,2,3]
     exec_port = 4
+    use_gpu = True
 
     def __init__(self, scene):
         super().__init__(scene, inputs=[4,6,1], outputs=[4,5,2,6,1])
@@ -71,13 +71,16 @@ class DiffBaseXLNode(AiNode):
         #assert token != "", "Token must be a valid HF Token string"
         #subprocess.call(["pip", "install", "git+https://github.com/patrickvonplaten/invisible-watermark.git@remove_onnxruntime_depedency"])
 
-        if not self.pipe:
-            self.pipe = self.getInputData(0)
-            if not self.pipe:
-                self.pipe = StableDiffusionXLPipeline.from_pretrained(self.path, torch_dtype=torch.float16, safety_checker=None, requires_safety_checker=False)
-                self.pipe.disable_attention_slicing()
-        self.pipe.to("cuda")
-        self.pipe.watermark.apply_watermark = dont_apply_watermark
+        # if not self.pipe:
+        #     self.pipe = self.getInputData(0)
+        #     if not self.pipe:
+        #         self.pipe = StableDiffusionXLPipeline.from_pretrained(self.path, torch_dtype=torch.float16, safety_checker=None, requires_safety_checker=False)
+        #         self.pipe.disable_attention_slicing()
+        pipe = self.getInputData(0)
+        gpu_id = self.content.gpu_id.currentText()
+        from ainodes_frontend import singleton as gs
+        pipe.to(f"{gs.device}:{gpu_id}")
+        pipe.watermark.apply_watermark = dont_apply_watermark
         prompt = self.content.prompt.toPlainText()
         prompt_2 = self.content.prompt_2.toPlainText()
         prompt_2 = prompt if prompt_2 == "" else prompt_2
@@ -89,7 +92,6 @@ class DiffBaseXLNode(AiNode):
         negative_prompt = self.content.n_prompt.toPlainText()
         negative_prompt_2 = self.content.n_prompt_2.toPlainText()
         negative_prompt_2 = negative_prompt if negative_prompt_2 == "" else negative_prompt_2
-
 
         data = self.getInputData(1)
         if data is not None:
@@ -108,7 +110,7 @@ class DiffBaseXLNode(AiNode):
         latents = None
         scheduler_name = self.content.scheduler_name.currentText()
         scheduler_enum = SchedulerType(scheduler_name)
-        self.pipe = get_scheduler(self.pipe, scheduler_enum)
+        pipe = get_scheduler(pipe, scheduler_enum)
 
         guidance_rescale = self.content.guidance_rescale.value()
         original_size = (self.content.original_width.value(), self.content.original_height.value())
@@ -116,7 +118,7 @@ class DiffBaseXLNode(AiNode):
 
         return_type = self.content.return_type.currentText()
 
-        image = self.pipe(  prompt = prompt,
+        image = pipe(  prompt = prompt,
                             prompt_2=prompt_2,
                             height = height,
                             width = width,
@@ -140,16 +142,14 @@ class DiffBaseXLNode(AiNode):
             "negative_prompt":negative_prompt,
             "negative_prompt_2":negative_prompt,
             "denoising_end":denoising_end
-
-
         }
-        self.pipe.to("cpu")
+        pipe.to("cpu")
         torch_gc()
         if return_type == "pil":
             tensor = pil2tensor(image)
-            return [self.pipe, [tensor], [None], data]
+            return [pipe, [tensor], [None], data]
         else:
-            return [self.pipe, [None], [image], data]
+            return [pipe, [None], [image], data]
 
 
     def remove(self):
